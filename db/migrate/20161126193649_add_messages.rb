b8782e3d77084291df22c9568ce9a3c7f5b200e8
greggawatt <greggahorton@gmail.com>
2016-11-26 11:47:58 -0800

    message, message severity and message category migrations + models

---
+++ db/migrate/20161126193649_add_messages.rb
@@ -0,0 +1,12 @@
+class AddMessages < ActiveRecord::Migration[5.0]
+  def change
+    create_table :messages do |t|
+      t.string :title
+      t.text :content, :limit => 1000000000
+      t.integer :message_category_id
+      t.integer :severity_id
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20161126194100_add_message_severity.rb
@@ -0,0 +1,9 @@
+class AddMessageSeverity < ActiveRecord::Migration[5.0]
+  def change
+    create_table :message_severities do |t|
+      t.string :title
+      
+      t.timestamps
+    end
+  end
+end
