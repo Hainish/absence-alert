d3e3eae6de88c797d5805d9b1d0ee35704396ebb
William Budington <bill@eff.org>
2016-11-26 16:31:42 -0800

    Rails reordering db/schema after merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,17 @@ ActiveRecord::Schema.define(version: 20161126211805) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "contacts", force: :cascade do |t|
+    t.datetime "created_at",                                     null: false
+    t.datetime "updated_at",                                     null: false
+    t.string   "email",                                          null: false
+    t.string   "confirmation_token", limit: 128
+    t.boolean  "confirmed",                      default: false
+    t.boolean  "rejected",                       default: false
+    t.integer  "user_id",                                        null: false
+    t.index ["email"], name: "index_contacts_on_email", using: :btree
+  end
+
   create_table "message_categories", force: :cascade do |t|
     t.string   "title"
     t.time     "send_at"
@@ -31,17 +42,6 @@ ActiveRecord::Schema.define(version: 20161126211805) do
     t.boolean  "active"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
-  end
-
-  create_table "contacts", force: :cascade do |t|
-    t.datetime "created_at",                                     null: false
-    t.datetime "updated_at",                                     null: false
-    t.string   "email",                                          null: false
-    t.string   "confirmation_token", limit: 128
-    t.boolean  "confirmed",                      default: false
-    t.boolean  "rejected",                       default: false
-    t.integer  "user_id",                                        null: false
-    t.index ["email"], name: "index_contacts_on_email", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
